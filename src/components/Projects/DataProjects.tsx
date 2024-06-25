abbd3e1be1117a804ac38f3ed16f4b2cf2a64bfe
KIGAMEKUN <reksa.prayoga1012@gmail.com>
2024-06-25 19:39:41 +0700

    feat(init first website)

---
+++ src/components/Projects/DataProjects.tsx
@@ -0,0 +1,198 @@
+import { useEffect, useRef, useState } from "react";
+import { motion, useAnimation, useInView } from "framer-motion";
+
+import '../../assets/styles/projects.css';
+import Modal from '../Modal/Modal';
+
+import etwpad from '../../assets/images/etwpad.png';
+import plab from '../../assets/images/plab.png';
+import cctv from '../../assets/images/cctv.png';
+import cinema from '../../assets/images/cinema.png';
+import redhunter from '../../assets/images/redhunter-page.jpg';
+import scindapsus from '../../assets/images/scindapsus.png';
+import scodeHotel from '../../assets/images/scode-hotel.png';
+import smk3 from '../../assets/images/smk3.png';
+import plantsasri from '../../assets/images/plantsasri.png';
+import lelanginandroid2 from '../../assets/images/lelanginandroid2.jpg';
+import lelanginandroid1 from '../../assets/images/lelanginandroid1.jpg';
+import img1 from '../../assets/images/1.png';
+import img4 from '../../assets/images/4.png';
+import marketplace from '../../assets/images/marketplace.png';
+import bkad from '../../assets/images/bkad.png';
+import isekaiHotel from '../../assets/images/isekai-hotel.png';
+import dmp from '../../assets/images/dmp.png';
+import dpr from '../../assets/images/dpr.png';
+import refmed from '../../assets/images/refmed.png';
+import mpp from '../../assets/images/mpp.jpg';
+
+import laravelLogo from '../../assets/icons/tech-stack/laravel.svg';
+import reactLogo from '../../assets/icons/tech-stack/react.svg';
+import postgreeLogo from '../../assets/icons/tech-stack/postgresql.svg';
+import bootstrapLogo from '../../assets/icons/tech-stack/bootstrap.svg';
+import mysqlLogo from '../../assets/icons/tech-stack/mysql.svg';
+import firebaseLogo from '../../assets/icons/tech-stack/firebase.svg';
+import midtransLogo from '../../assets/icons/tech-stack/MIDTRANS.jpg';
+import StripeLogo from '../../assets/icons/tech-stack/stripe.svg';
+import PaypallLogo from '../../assets/icons/tech-stack/paypal.svg';
+import apacheLogo from '../../assets/icons/tech-stack/apache.svg';
+import nginxLogo from '../../assets/icons/tech-stack/nginx.svg';
+import tailwind from '../../assets/icons/tech-stack/tailwind.svg';
+import express from '../../assets/icons/tech-stack/express.svg';
+import next from '../../assets/icons/tech-stack/next.svg';
+import mongodb from '../../assets/icons/tech-stack/mongodb.svg';
+import flutter from '../../assets/icons/tech-stack/flutter.svg';
+import flask from '../../assets/icons/tech-stack/flask.svg';
+import ant from '../../assets/icons/tech-stack/antdesign.svg';
+import xendit from '../../assets/icons/tech-stack/xendit.svg';
+
+const DataProjects: React.FC = () => {
+
+  const images = [
+    { src: etwpad, tech: [laravelLogo, bootstrapLogo, postgreeLogo, nginxLogo], gridArea: 'itgrid one shadow md:col-span-2 md:row-span-2' },
+    { src: bkad, tech: [laravelLogo, bootstrapLogo, mysqlLogo, apacheLogo], gridArea: 'itgrid two shadow md:col-span-2 md:row-span-1' },
+    { src: scodeHotel, tech: [laravelLogo, tailwind, mysqlLogo, apacheLogo], gridArea: 'itgrid three shadow md:col-span-2 md:row-span-1' },
+
+    { src: plab, tech: [laravelLogo, bootstrapLogo, mysqlLogo, apacheLogo], gridArea: 'itgrid four shadow md:col-span-2 md:row-span-1' },
+    { src: redhunter, tech: [express, next, tailwind, mongodb, nginxLogo], gridArea: 'itgrid five shadow md:col-span-2 md:row-span-2 bg-top' },
+    { src: smk3, tech: [laravelLogo, bootstrapLogo, mysqlLogo], gridArea: 'itgrid six shadow md:col-span-2 md:row-span-1' },
+
+    { src: plantsasri, tech: [laravelLogo, bootstrapLogo, firebaseLogo, mysqlLogo, apacheLogo, midtransLogo, StripeLogo, PaypallLogo], gridArea: 'itgrid seven md:col-span-2 md:row-span-2 bg-top shadow' },
+    { src: img1, tech: [laravelLogo, bootstrapLogo, flutter, mysqlLogo, apacheLogo, midtransLogo], gridArea: 'itgrid eight md:col-span-2 md:row-span-1 shadow' },
+    { src: img4, tech: [laravelLogo, bootstrapLogo, flutter, mysqlLogo, apacheLogo, midtransLogo], gridArea: 'itgrid nine md:col-span-2 md:row-span-1 shadow' },
+
+    { src: marketplace, tech: [laravelLogo, bootstrapLogo, mysqlLogo, firebaseLogo, midtransLogo, StripeLogo, PaypallLogo], gridArea: 'itgrid ten md:col-span-2 md:row-span-1 bg-top shadow' },
+    { src: lelanginandroid2, tech: [laravelLogo, bootstrapLogo, flutter, mysqlLogo, midtransLogo], gridArea: 'itgrid eleven md:col-span-1 md:row-span-2 shadow' },
+    { src: lelanginandroid1, tech: [laravelLogo, bootstrapLogo, flutter, mysqlLogo, midtransLogo], gridArea: 'itgrid twelve md:col-span-1 md:row-span-2 shadow' },
+    { src: cinema, tech: [laravelLogo, tailwind, mysqlLogo, apacheLogo], gridArea: 'itgrid thirteen md:col-span-2 md:row-span-1 shadow' },
+
+    { src: scindapsus, tech: [laravelLogo, bootstrapLogo, mysqlLogo, apacheLogo], gridArea: 'itgrid fourteen md:col-span-2 md:row-span-2 bg-top shadow' },
+    { src: dmp, tech: [flask, reactLogo, tailwind, postgreeLogo, nginxLogo], gridArea: 'itgrid fiveteen md:col-span-2 md:row-span-1 bg-top shadow' },
+    { src: dpr, tech: [next, ant, postgreeLogo, nginxLogo], gridArea: 'itgrid sixteen md:col-span-2 md:row-span-1 bg-top shadow' },
+
+
+    { src: refmed, tech: [laravelLogo, bootstrapLogo, mysqlLogo, firebaseLogo, apacheLogo], gridArea: 'itgrid seventeen md:col-span-2 md:row-span-1 bg-top shadow' },
+    { src: mpp, tech: [flutter], gridArea: 'itgrid  eighteen md:col-span-1 md:row-span-2 shadow' },
+    { src: cctv, tech: [laravelLogo, bootstrapLogo, , mysqlLogo, apacheLogo], gridArea: 'itgrid nineteen md:col-span-1 md:row-span-2 shadow' },
+    { src: isekaiHotel, tech: [laravelLogo, reactLogo, bootstrapLogo, apacheLogo, xendit], gridArea: 'itgrid twenty md:col-span-2 md:row-span-2 shadow' },
+
+  ];
+
+
+  const ref = useRef(null);
+
+  const isScroll = useInView(ref, { once: false });
+  const aboutContentControls = useAnimation();
+
+  const dataProjectsVariants = {
+    hidden: { opacity: 0, y: 12 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delayChildren: 0.5,
+        staggerChildren: 0.2,
+        duration: 0.5,
+        delay: 0.25,
+        type: "spring",
+        stiffness: 80,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { x: 12, opacity: 0 },
+    visible: {
+      opacity: 1,
+      x: 0,
+      duration: {
+        type: "spring",
+        stiffness: 100,
+      },
+    },
+  };
+
+  const imagesPerPage = [6, 7, 7]; // Jumlah gambar per halaman
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = imagesPerPage.length; // Jumlah total halaman
+
+  const handlePrevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
+
+  const currentImages = images.slice(
+    imagesPerPage.slice(0, currentPage - 1).reduce((acc, val) => acc + val, 0),
+    imagesPerPage.slice(0, currentPage).reduce((acc, val) => acc + val, 0)
+  );
+
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  useEffect(() => {
+    if (isScroll) {
+      aboutContentControls.start("visible");
+    }
+  }, [isScroll]);
+
+  return (
+
+    <motion.div
+      ref={ref}
+      animate={aboutContentControls}
+      initial="hidden"
+      variants={dataProjectsVariants}
+      className="w-full mx-auto mt-10"
+    >
+
+      <div className="gallery grid gap-5 grid-cols-4 sm:grid-rows-[repeat(6,1fr)] h-[1400px] md:grid-cols-4 md:grid-rows-[repeat(4,1fr)]">
+        {currentImages.map((image, index) => (
+          <div
+            key={index}
+            className={`relative bg-cover bg-center rounded-xl flex justify-center items-center transition duration-500 ease-in-out group ${image.gridArea}`}
+            style={{ backgroundImage: `url(${image.src})` }}
+          >
+            <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 rounded-xl transition duration-500 ease-in-out"></div>
+            <div className="absolute bottom-2 right-2 flex gap-2 space-x-2 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out bg-white rounded px-2 py-1">
+              {image.tech.map((techLogo, techIndex) => (
+                <img key={techIndex} src={techLogo} alt="Tech Logo" className="h-8 w-8" />
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-center mt-4 space-x-4">
+        <button
+          onClick={handlePrevPage}
+          disabled={currentPage === 1}
+          className="px-4 py-2 bg-blue-500 text-white rounded disabled:bg-gray-300"
+        >
+          Previous
+        </button>
+        <button
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages}
+          className="px-4 py-2 bg-blue-500 text-white rounded disabled:bg-gray-300"
+        >
+          Next
+        </button>
+      </div>
+
+
+    </motion.div>
+
+
+  );
+};
+
+export default DataProjects;
---
+++ src/constants/index.ts
@@ -0,0 +1,208 @@
+// Import assets social media
+// import instagram from "../assets/icons/social-media/instagram.svg";
+import medium from "../assets/icons/social-media/medium.webp";
+import linkedin from "../assets/icons/social-media/linkedin.webp";
+import github from "../assets/icons/social-media/github.webp";
+import email from "../assets/icons/social-media/email.webp";
+
+// Import assets projects and skills
+import villageWeb from "../assets/images/village-web.webp";
+import hmtiUntad from "../assets/images/hmti-untad.webp";
+import hrev from "../assets/images/hrev.webp";
+import snResidence from "../assets/images/sn-residence.webp";
+
+import logoVillageWebsite from "../assets/icons/logo-village-website.png";
+import logoHmtiUntad from "../assets/icons/logo-hmti-untad.png";
+import logoHrev from "../assets/icons/logo-hrev.png";
+import logoSnResidence from "../assets/icons/logo-sn-residence.png";
+
+
+
+
+import laravel from "../assets/icons/tech-stack/laravel.svg";
+import go from "../assets/icons/tech-stack/go.svg";
+import flutter from "../assets/icons/tech-stack/flutter.svg";
+import express from "../assets/icons/tech-stack/express.svg";
+import python from "../assets/icons/tech-stack/python.svg";
+import mysql from "../assets/icons/tech-stack/mysql.svg";
+import node from "../assets/icons/tech-stack/node.svg";
+import nginx from "../assets/icons/tech-stack/nginx.svg";
+import flask from "../assets/icons/tech-stack/flask.svg";
+import django from "../assets/icons/tech-stack/django.svg";
+import php from "../assets/icons/tech-stack/php.svg";
+import next from "../assets/icons/tech-stack/next.svg";
+
+
+
+import nextjs from "../assets/icons/tech-stack/nextjs.svg";
+import react from "../assets/icons/tech-stack/react.svg";
+import typescript from "../assets/icons/tech-stack/typescript.svg";
+import javascript from "../assets/icons/tech-stack/javascript.svg";
+import redux from "../assets/icons/tech-stack/redux.svg";
+import graphql from "../assets/icons/tech-stack/graphql.svg";
+import nodejs from "../assets/icons/tech-stack/nodejs.svg";
+import tailwind from "../assets/icons/tech-stack/tailwind.svg";
+import antdesign from "../assets/icons/tech-stack/antdesign.svg";
+import mongodb from "../assets/icons/tech-stack/mongodb.svg";
+import postgresql from "../assets/icons/tech-stack/postgresql.svg";
+import firebase from "../assets/icons/tech-stack/firebase.svg";
+import vite from "../assets/icons/tech-stack/vite.svg";
+
+import bonet from "../assets/icons/company-logo/bonet.jpg";
+import redhunter from "../assets/icons/company-logo/redhunter.jpg";
+import scode from "../assets/icons/company-logo/scode.png";
+import kreasi from "../assets/icons/company-logo/kreasi.svg";
+
+import pattern1 from "../assets/images/patterns/pattern1.svg";
+import pattern2 from "../assets/images/patterns/pattern2.svg";
+import pattern3 from "../assets/images/patterns/pattern3.svg";
+import pattern4 from "../assets/images/patterns/pattern4.svg";
+
+export const dataNavbar: Navbar[] = [
+  {
+    id: 0,
+    navigate: "Profile",
+    navigate_url: "profile",
+    offset: -100,
+  },
+  {
+    id: 1,
+    navigate: "Projects",
+    navigate_url: "projects",
+    offset: -105,
+  },
+  {
+    id: 2,
+    navigate: "Experience",
+    navigate_url: "experience",
+    offset: -50,
+  },
+  {
+    id: 3,
+    navigate: "Contact",
+    navigate_url: "contact",
+    offset: 0,
+  },
+];
+
+export const dataLogoSocialMedia: LogoSocialMedia[] = [
+  {
+    id: 0,
+    logo: linkedin,
+    navigate: "https://www.linkedin.com/in/reksa-prayoga-syahputra/",
+  },
+  {
+    id: 1,
+    logo: medium,
+    navigate: "https://medium.com/@vickyadri29",
+  },
+  {
+    id: 2,
+    logo: github,
+    navigate: "https://github.com/Kigamekun",
+  },
+  {
+    id: 3,
+    logo: email,
+    navigate: "mailto:reksa.prayoga1012@gmail.com",
+  },
+];
+
+export const dataSkills: string[] = [
+  laravel,
+  flask,
+  django,
+  express,
+  node,
+  php,
+  python,
+  mongodb,
+  postgresql,
+  mysql,
+  nginx,
+  flutter,
+  go,
+  react,
+  typescript,
+  javascript,
+  firebase,
+  next,
+
+
+];
+
+
+
+export const dataExperience: Experience[] = [
+  {
+    id: 0,
+    logo_url: redhunter,
+    pattern: pattern4, // Belum ada informasi tentang pattern untuk gambar ini
+    name: "Redhunter Academy",
+    description: "@redhunter",
+    date: "Aug 2021 - Mar 2023",
+    position: "Freelancer",
+    responsibilites: [
+      "Built a learning management system for Redhunter Academy to provide learning classes.",
+    ],
+    first_color: "#2E0D17",
+    second_color: "#390916",
+    desc_color: "#F9B6BE",
+    position_color: "#18040A",
+    border_color: "#7D2038",
+  },
+  {
+    id: 1,
+    logo_url: scode,
+    pattern: pattern2, // Belum ada informasi tentang pattern untuk gambar ini
+    name: "Startcode ID",
+    description: "@startcode",
+    date: "Jul 2021 - Present",
+    position: "CTO",
+    responsibilites: [
+      "Developed various applications such as Point of Sale System, inventory, company profile, e-commerce, etc.",
+      "Provided high quality services focusing on responsiveness and creativity.",
+    ],
+    first_color: "#001F33",
+    second_color: "#002C4B",
+    desc_color: "#A7D6F7",
+    position_color: "#001325",
+    border_color: "#005EB8"
+  },
+  {
+    id: 2,
+    logo_url: kreasi,
+    pattern: '', // Belum ada informasi tentang pattern untuk gambar ini
+    name: "Visi Kreasi Asia",
+    description: "@kreasi",
+    date: "Jul 2021 - Jul 2022",
+    position: "Fullstack Developer",
+    responsibilites: [
+      "Led and managed the development team, especially in backend divisions.",
+      "Digitized financial applications for the Indonesian Army.",
+    ],
+    first_color: "#520E27",
+    second_color: "#6F133A",
+    desc_color: "#F3A7B3",
+    position_color: "#3D0A18",
+    border_color: "#A82040"
+  },
+  {
+    id: 3,
+    logo_url: bonet,
+    pattern: pattern4,
+    name: "Bonet Utama",
+    description: "@bonetutama",
+    date: "Jan 2022 - Oct 2023",
+    position: "Fullstack Developer",
+    responsibilites: [
+      "Interned as a Software Engineering intern at PT. BONET UTAMA.",
+      "Worked on several applications including SMKN 3 Bogor City's web, LSP SMKN 3 Bogor City's web, and CCTV DLLAJ Web.",
+    ],
+    first_color: "#01271C",
+    second_color: "#08573E",
+    desc_color: "#C6F8B1",
+    position_color: "#033424",
+    border_color: "#10AA7A",
+  },
+];
